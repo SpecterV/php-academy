fb11e9dbf483c7fa97a3accc079ae04244902730
Oleksandr Velykyi <a.a.velikiy@gmail.com>
2016-11-29 10:44:28 +0200

    Optimization tasks 16,25

--- arrays_loops_tasks/16.php
+++ arrays_loops_tasks/16.php
@@ -6,23 +6,27 @@
 7, 8, 9</p>*/
 $arr = [1,2,3,4,5,6,7,8,9];
 foreach($arr as $value) {
-    if ($value <= 2) {
-        echo "$value, ";
-    } elseif ($value == 3) {
-        echo "$value";
-        echo "<br>";
-    }
-    if ($value > 3 && $value <= 5) {
-        echo "$value, ";
-    } elseif ($value == 6) {
-        echo "$value";
-        echo "<br>";
-    }
-    if ($value > 6 && $value <= 8) {
-        echo "$value, ";
-    } elseif ($value == 9) {
-        echo "$value";
-    }
+    echo "$value, ";
+        if ($value % 3 == 0) {
+            echo "<br>";
+        }
+//    if ($value <= 2) {
+//        echo "$value, ";
+//    } elseif ($value == 3) {
+//        echo "$value";
+//        echo "<br>";
+//    }
+//    if ($value > 3 && $value <= 5) {
+//        echo "$value, ";
+//    } elseif ($value == 6) {
+//        echo "$value";
+//        echo "<br>";
+//    }
+//    if ($value > 6 && $value <= 8) {
+//        echo "$value, ";
+//    } elseif ($value == 9) {
+//        echo "$value";
+//    }
 }
 /**
  * Created by PhpStorm.
