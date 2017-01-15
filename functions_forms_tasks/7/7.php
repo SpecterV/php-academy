d225518271de31cb78e203e9d12aaa9eb1855c95
Oleksandr Velykyi <a.a.velikiy@gmail.com>
2017-01-15 16:01:56 +0200

    homeworks updated

--- functions_forms_tasks/7/7.php
+++ functions_forms_tasks/7/7.php
@@ -4,7 +4,7 @@ require('function.php');
 if (empty($_POST['comment'])) {
     echo guestForm();
 } else {
-    file_put_contents('guestBook.txt', "\n" . $_POST['comment'] . file_get_contents('guestBook.txt'));
+    file_put_contents('guestBook.txt', "\n" . strip_tags($_POST['comment']) . file_get_contents('guestBook.txt'));
     $contents = file_get_contents('guestBook.txt');
     echo "<pre>";
     echo $contents;
